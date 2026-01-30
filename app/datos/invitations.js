c0112f0e1957c3ec250c35a07955ae8fcf5dac31
anabmiranda-dev <anabmira11@gmail.com>
2026-01-30 20:26:38 -0300

    cambio de fotos y agrego un ejemplo en invitaciones

--- app/datos/invitations.js
+++ app/datos/invitations.js
@@ -8,9 +8,16 @@ export const invitations = [
   },
   {
     id: 2,
-    title: "Christian y Cecilia",
+    title: "Ani y Cami",
     category: "Cumpleaños",
     image: "/invitacion-2.png",
     link: "https://ejemplo-demo-aniycami.web.app/",
+  },
+    {
+    id: 3,
+    title: "Lu y Emi",
+    category: "Cumpleaños",
+    image: "/invitacion-3.png",
+    link: "https://invitacionesdemo-luyemi.web.app/",
   },
 ];
