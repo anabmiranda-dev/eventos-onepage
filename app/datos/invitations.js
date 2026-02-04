cccfa77aaa4e870917e4b1bcc86ca15e31043581
anabmiranda-dev <anabmira11@gmail.com>
2026-02-04 09:27:19 -0300

    Agrego un evento

--- app/datos/invitations.js
+++ app/datos/invitations.js
@@ -9,15 +9,22 @@ export const invitations = [
   {
     id: 2,
     title: "Ani y Cami",
-    category: "Cumpleaños",
+    category: "Boda",
     image: "/invitacion-2.png",
     link: "https://ejemplo-demo-aniycami.web.app/",
   },
     {
     id: 3,
     title: "Lu y Emi",
-    category: "Cumpleaños",
+    category: "Boda",
     image: "/invitacion-3.png",
     link: "https://invitacionesdemo-luyemi.web.app/",
+  },
+    {
+    id: 4,
+    title: "Olivia y Pedro",
+    category: "Boda",
+    image: "/invitacion-4.jpeg",
+    link: "https://invitacionesdemo-oliviaypedro.web.app/",
   },
 ];
